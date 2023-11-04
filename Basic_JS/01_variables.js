797628d88ab82363d7ad9652e845cdf57773d7ec
soumik51 <119774026+soumik51@users.noreply.github.com>
2023-11-04 19:26:20 +0000

    container config done

---
+++ Basic_JS/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accoutEmail = "soumik@facebook.com"
+var accountPassword = "221155"
+accountCity = " India"
+let accountAddress
+
+accoutEmail= "soumik@google.com"
+// accountId = 4
+console.log(accountId);
+console.table([accountId, accoutEmail,accountPassword,accountCity, accountAddress])
