eef3f76abb740829e0ec80316173cba4ba6af118
soumik51 <119774026+soumik51@users.noreply.github.com>
2023-11-09 09:37:25 +0000

    conversion of datatype

---
+++ Basic_JS/datatype.js
@@ -0,0 +1,21 @@
+            /*Datatypes Conversion*/
+
+// let score = "soumik"//true//null //"44a"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let isLoggedIn= " "
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let ifnumber= 70
+
+let thisNumber= String(ifnumber)
+console.log(thisNumber);
+console.log(typeof thisNumber);
